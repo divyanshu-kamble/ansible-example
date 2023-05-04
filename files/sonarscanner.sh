17041ebcb0fb65d4c2d258c9a1967664288db667
Divyanshu K <divyanshu.kamble11@gmail.com>
2023-05-04 19:39:12 +0530

    added sonarscanner installation

---
+++ files/sonarscanner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /opt/
+
+wget -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.7.0.2747-linux.zip -O /opt/sonarscanner.zip
+
+unzip sonarscanner.zip
+
+rm sonarscanner.zip
+
+sudo mv sonar-scanner-cli-4.7.0.2747-linux  sonarqube
+
