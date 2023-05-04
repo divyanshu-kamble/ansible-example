e5ffd69e4cf6cecb348927e8f7fba14b801f9a8a
Divyanshu K <divyanshu.kamble11@gmail.com>
2023-05-04 12:14:39 +0530

    sonar installation

--- files/sonar.sh
+++ files/sonar.sh
@@ -6,7 +6,8 @@ cd /opt/
 # sudo unzip sonarqube-9.9.0.65466.zip
 
 # sudo echo '{\n   RUN_AS_USER="duser"\n}' > /opt/sonarqube-9.9.0.65466/bin/linux-x86-64/sonar.sh 
-/sonarqube-9.9.0.65466/bin/linux-x86-64/sonar.sh start
+
+sudo /sonarqube-9.9.0.65466/bin/linux-x86-64/sonar.sh start
 
 
 
