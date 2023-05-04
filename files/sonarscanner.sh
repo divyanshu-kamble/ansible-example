91de0f93e00814bf5c1ac4d2aa2a7d4aa510a451
Divyanshu K <divyanshu.kamble11@gmail.com>
2023-05-04 20:01:37 +0530

    sonarscanner script

--- files/sonarscanner.sh
+++ files/sonarscanner.sh
@@ -7,7 +7,12 @@ unzip sonarscanner.zip
 
 rm sonarscanner.zip
 
+sed -i 's/#sonar.host.url=http://localhost:9000/sonar.host.url=http://localhost:9001/g' /opt/sonar-scanner-4.7.0.2747-linux/conf/sonar-scanner.properties
+
 sudo mv sonar-scanner-cli-4.7.0.2747-linux  sonarscanner
 
 export PATH="$PATH:/opt/sonarscanner/bin"
+
+
+
 
